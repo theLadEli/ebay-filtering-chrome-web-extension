fdb987303424f4bc8718fef4d58a230a5e5531b1
Eli <109231330+theLadEli@users.noreply.github.com>
2023-04-05 13:48:48 +0100

    updated ebay filtering extension

--- content.js
+++ content.js
@@ -1,42 +1,20 @@
 const includePhrases = document.getElementById("includePhrases").value;
 const excludePhrases = document.getElementById("excludePhrases").value;
 
-const filterButton = document.getElementById("filterButton");
-filterButton.addEventListener("click", filterResults);
+let url = "https://www.ebay.com/sch/i.html?_nkw=";
 
-function filterResults() {
-    const includePhrasesArray = includePhrases.split(",");
-    const excludePhrasesArray = excludePhrases.split(",");
-
-    const items = document.querySelectorAll(".s-item_link");
-    items.forEach(item => {
-        fetch(item.href)
-            .then(response => response.text())
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, "text/html");
-                const description = doc.querySelector(".d-item-description").textContent;
-
-                let includePhraseFound = false;
-                let excludePhraseFound = false;
-
-                includePhrasesArray.forEach(phrase => {
-                    if (description.includes(phrase)) {
-                        includePhraseFound = true;
-                    }
-                });
-
-                excludePhrasesArray.forEach(phrase => {
-                    if (description.includes(phrase)) {
-                        excludePhraseFound = true;
-                    }
-                });
-
-                if (includePhraseFound && !excludePhraseFound) {
-                    item.closest(".s-item").style.display = "block";
-                } else {
-                    item.closest(".s-item").style.display = "none";
-                }
-            });
-    });
+if (includePhrases) {
+  const includePhrasesList = includePhrases.split(",");
+  includePhrasesList.forEach(phrase => {
+    url += `&_in_kw=${phrase.trim()}`;
+  });
 }
+
+if (excludePhrases) {
+  const excludePhrasesList = excludePhrases.split(",");
+  excludePhrasesList.forEach(phrase => {
+    url += `&_ex_kw=${phrase.trim()}`;
+  });
+}
+
+console.log("Filtered URL:", url);
